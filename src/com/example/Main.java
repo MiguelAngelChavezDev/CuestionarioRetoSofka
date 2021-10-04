f5215f45c3c4524dc32094c4ed4c6c59cea92c39
Miguel Angel Chavez <ingenieria.mc01@gmail.com>
2021-10-04 01:15:58 -0500

    EstructuraNivel1Complete

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,19 +1,19 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
+        //Ronda # 1 Nivel Facil
         Pregunta[] preguntasFacil1;
-        preguntasFacil1 = new Pregunta[1];
-        preguntasFacil1[0] = new Pregunta("¿Cuál es la fórmula química del agua?\n\n", "a) H2O\n", "b) CO2\n", "c) HO2\n", "d) CH2\n", "a");
-
-        System.out.println(preguntasFacil1[0].getRespuestaCorrecta());
+        preguntasFacil1 = new Pregunta[5];
+        preguntasFacil1[0] = new Pregunta("¿Cuál es la fórmula química del agua?\n\n", "a) H2O\n", "b) CO2\n", "c) HO2\n", "d) CH2\n", "1");
+        preguntasFacil1[1] = new Pregunta("¿Qué es un gecko?\n\n", "a) Un Instrumento Musical\n", "b) Un Reptil\n", "c) Un Astro\n", "d) Un Artrópodo\n", "b");
+        preguntasFacil1[2] = new Pregunta("¿Cuál órgano celular se encarga de producir energía en la célula?\n\n", "a) Ribosoma\n", "b) Libosoma\n", "c) Mitocondria\n", "d) Aparato de Golgi\n", "c");
+        preguntasFacil1[3] = new Pregunta("¿Quién escribió Cien años de soledad?\n\n", "a) Rafael Pombo\n", "b) Jorge Isaacs\n", "c) José Asunción Silva\n", "d) Gabriel García Márquez\n", "d");
+        preguntasFacil1[4] = new Pregunta("¿El conjunto de instrucciones ordenadas para ejecutar tareas ¿Se le llama?\n\n", "a) Algoritmo\n", "b) Autómata\n", "c) Ordenador\n", "d) Código\n", "a");
 
         //Declaring some variables and range of random questions
         int max = 4;
@@ -44,67 +44,18 @@ public class Main {
 
         //Conditionals
         if (resUser.length() == 1) {
-            if (resUser == preguntaActual.respuestaCorrecta) {
+            if (preguntaActual.getRespuestaCorrecta().equals(resUser)) {
+                System.out.println("RESPUESTA CORRECTA");
+                System.out.println("HAZ AVANZADO A LA SIGUIENTE RONDA");
                 premioUsuario.acumulado = premioUsuario.premioNivel1;
-                System.out.println("Respuesta");
-                System.out.println("usted ha ganado XX puntos");
-                System.out.println("usted ha ganado XX puntos");
+                System.out.println("TU PUNTAJE ACUMULADO ES DE " + premioUsuario.acumulado);
             } else {
+                System.out.println("JUEGO TERMINADO");
                 premioUsuario.acumulado = 0;
-                System.out.println("Juego terminado");
+                System.exit(0);
             }
         } else {
             System.out.println("Debe ingresar solamente letras a, b, c, d");
         }
     }
 }
-
-    //Ronda 2 - Preguntas Basico
-//    List<Pregunta> preguntasBasico = new ArrayList<>();
-//
-//        preguntasBasico.add(new
-//
-//    Pregunta("¿Cuál es la fórmula química del agua?\n\n","a) H2O\n","b) CO2\n","c) HO2\n","d) CH2\n","a"));
-//        preguntasBasico.add(new
-//
-//    Pregunta("¿Qué es un gecko?\n\n","a) Un Instrumento Musical\n","b) Un Reptil\n","c) Un Astro\n","d) Un Artrópodo\n","b"));
-//        preguntasBasico.add(new
-//
-//    Pregunta("¿Cuál órgano celular se encarga de producir energía en la célula?\n\n","a) Ribosoma\n","b) Libosoma\n","c) Mitocondria\n","d) Aparato de Golgi\n","c\n"));
-//        preguntasBasico.add(new
-//
-//    Pregunta("¿Quién escribió Cien años de soledad?\n\n","a) Rafael Pombo\n","b) Jorge Isaacs\n","c) José Asunción Silva\n","d) Gabriel García Márquez\n","d\n"));
-//        preguntasBasico.add(new
-//
-//    Pregunta("¿El conjunto de instrucciones ordenadas para ejecutar tareas ¿Se le llama??\n\n","a) Algoritmo\n","b) Autómata\n","c) Ordenador\n","d) Código\n","a\n"));
-//
-//}
-//
-//
-////        //PONER AQUI LAS CONDICIONES
-////
-////
-////
-////           //if(resUser == )
-////
-////
-////
-////
-////        //Generando Objeto Premio usuario 1
-////        Premios premioUsuario1 = new Premios();
-////
-////        premioUsuario1.acumulado = premioUsuario1.premioNivel1 + premioUsuario1.premioNivel2;
-////
-////        //System.out.println(premioUsuario1.acumulado);
-////
-////
-////        //System.out.println(user1.name);
-////
-////        preguntasDificil.add(new Pregunta("Cuanto es 3+3", "4", "5", "6", "8", "c"));
-////
-////
-////
-////
-////        }
-////    }
-//}
